419de4259417ce873bf5ded30be796bdadb74c91
rogan528 <3097940262@qq.com>
2018-12-31 22:37:44 +0800

    修改常量定义

--- app/src/main/java/com/zhangbin/utils/ConstantsValue.java
+++ app/src/main/java/com/zhangbin/utils/ConstantsValue.java
@@ -1,5 +1,7 @@
 package com.zhangbin.utils;
 
+import android.content.Context;
+
 import com.zhangbin.R;
 
 public class ConstantsValue {
@@ -7,6 +9,8 @@ public class ConstantsValue {
         throw new UnsupportedOperationException("不能被实例化");
     }
     public final static String server_url = "https://msp.alipay.com/x.htm";
+    public static final String XML_NAME = "share_date";
+    public static final int XML_MODLE = Context.MODE_PRIVATE;
     public final static int[] mGuideIamgeIds =new int[]{R.mipmap.guide_1,R.mipmap.guide_2,R.mipmap.guide_3};
     public enum XmlKeyName{
         FIRST_LOGIN
--- app/src/main/java/com/zhangbin/utils/SharedPreferencesUtils.java
+++ app/src/main/java/com/zhangbin/utils/SharedPreferencesUtils.java
@@ -14,7 +14,11 @@ public class SharedPreferencesUtils {
     /**
      * 保存在手机里面的文件名
      */
-    private static final String FILE_NAME = "share_date";
+    private static final String FILE_NAME = ConstantsValue.XML_NAME;
+    /**
+     * 保存在手机里面的模式
+     */
+    private static final int XML_MODLE = ConstantsValue.XML_MODLE;
 
     /**
      * 保存数据的方法，我们需要拿到保存数据的具体类型，然后根据类型调用不同的保存方法
@@ -26,19 +30,16 @@ public class SharedPreferencesUtils {
 
         String type = object.getClass().getSimpleName();
         String key = xmlKeyName.name();
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, XML_MODLE);
         SharedPreferences.Editor editor = sp.edit();
 
         if("String".equals(type)){
             editor.putString(key, (String)object);
-        }
-        else if("Integer".equals(type)){
+        } else if("Integer".equals(type)){
             editor.putInt(key, (Integer)object);
-        }
-        else if("Boolean".equals(type)){
+        } else if("Boolean".equals(type)){
             editor.putBoolean(key, (Boolean)object);
-        }
-        else if("Float".equals(type)){
+        } else if("Float".equals(type)){
             editor.putFloat(key, (Float)object);
         }
         else if("Long".equals(type)){
@@ -57,28 +58,19 @@ public class SharedPreferencesUtils {
     public static Object getParam(Context context , ConstantsValue.XmlKeyName xmlKeyName, Object defaultObject){
         String type = defaultObject.getClass().getSimpleName();
         String key = xmlKeyName.name();
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, XML_MODLE);
 
         if("String".equals(type)){
             return sp.getString(key, (String)defaultObject);
-        }
-
-        else if("Integer".equals(type)){
+        } else if("Integer".equals(type)){
             return sp.getInt(key, (Integer)defaultObject);
-        }
-
-        else if("Boolean".equals(type)){
+        } else if("Boolean".equals(type)){
             return sp.getBoolean(key, (Boolean)defaultObject);
-        }
-
-        else if("Float".equals(type)){
+        } else if("Float".equals(type)){
             return sp.getFloat(key, (Float)defaultObject);
-        }
-
-        else if("Long".equals(type)){
+        } else if("Long".equals(type)){
             return sp.getLong(key, (Long)defaultObject);
         }
-
         return null;
     }
 
@@ -88,7 +80,7 @@ public class SharedPreferencesUtils {
      */
     public static void clearAll(Context context) {
         SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
+                XML_MODLE);
         SharedPreferences.Editor editor = sp.edit();
         editor.clear().commit();
     }
@@ -98,8 +90,7 @@ public class SharedPreferencesUtils {
      * @param context
      */
     public static void clearXmlKey(Context context,ConstantsValue.XmlKeyName xmlKeyName) {
-        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, XML_MODLE);
         SharedPreferences.Editor editor = sp.edit();
         editor.remove(xmlKeyName.name());
         editor.commit();
